458217ea27a0abb768f2c5085272f2d48fd3f2de
Ernesto-Sanchez-J <56661723+Ernesto-Sanchez-J@users.noreply.github.com>
2020-06-06 00:36:34 -0500

    more components made

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { DateTimeComponent } from './date-time/date-time.component';
 import { MessagePreviewComponent } from './message-preview/message-preview.component';
+import { TaskItemComponent } from './task-item/task-item.component';
+import { TopNavComponent } from './top-nav/top-nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DateTimeComponent,
-    MessagePreviewComponent
+    MessagePreviewComponent,
+    TaskItemComponent,
+    TopNavComponent
   ],
   imports: [
     BrowserModule
