0b6924c189d9a5be3ab5fa61f2196588a6feeb22
Ernesto-Sanchez-J <56661723+Ernesto-Sanchez-J@users.noreply.github.com>
2020-06-07 12:16:19 -0500

    Finished transaction panel

--- src/app/transactions-panel/transactions-panel.component.ts
+++ src/app/transactions-panel/transactions-panel.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import orders, {} from '../orders';
 @Component({
   selector: 'app-transactions-panel',
   templateUrl: './transactions-panel.component.html',
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TransactionsPanelComponent implements OnInit {
 
+  orders;
   constructor() { }
 
   ngOnInit() {
+    this.orders = orders;
   }
 
 }
